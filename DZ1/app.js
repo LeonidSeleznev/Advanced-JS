6bec43c708b3845db694a025d604ca4d0fe778a9
Leonid <levon.seleznev@gmail.com>
2020-09-17 18:51:34 +0600

    DZ1 Refactored

--- DZ1/app.js
+++ DZ1/app.js
@@ -19,7 +19,7 @@ const renderProducts = (list) => {
     const productList = list.map((product) => {
         return renderProduct(product.title, product.price);
     });
-    document.querySelector('.products').innerHTML = productList;
+    document.querySelector('.products').innerHTML = productList.join('');
 };
 
 renderProducts(products);
